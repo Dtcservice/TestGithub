711e1d9d7dcd2e4f9763d984fdd92f9f4037acf0
Dtcservice <57124659+Dtcservice@users.noreply.github.com>
2019-10-30 09:12:52 +0700

    Update csv_test_2.php

--- csv_test_2.php
+++ csv_test_2.php
@@ -8,7 +8,7 @@ header('Content-type: application/csv; charset=UTF-8');
 header('Content-Disposition: attachment; filename='.$filename);
 $fp = fopen('php://output', 'w');
     
-    $header = array("กลุ่มซิม 12",
+    $header = array("กลุ่มซิม",
      "หมายเลขซิม", 
      "ค่าแพคเกจ" , 
      "ยอดรวมค่าใช้จ่าย" ,
